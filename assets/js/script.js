394c83ecf8a1cb62fd7115cd64622f03a1711d50
evanbruce38 <106777653+evanbruce38@users.noreply.github.com>
2022-07-05 22:13:19 -0400

    added quiz questions and quiz function

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,10 +20,108 @@ var delayInMilliseconds = 2500;
 var minutes = 0;
 var seconds = 0;
 var timer = null;
-var currentHighScore = 0;
+var currentUserScore = 0;
 var highScores = [];
 
 // Array of questions //
 var questionList = [
-    
-]
+    {
+        questionNumber: 1
+        question: "What is github?"
+        options: [
+            "An online repository".
+            "A blog",
+            "A travel agency",
+            "An online computer store",
+        ],
+        correctAnswer: "An online repository",
+    },
+    {
+        questionNumber: 2
+        question: "What does (ul) stand for in HTML?"
+        options: [
+            "Uneven lanes".
+            "Urgent lead",
+            "Unequal levels",
+            "Unordered list",
+        ],
+        correctAnswer: "Unordered list",
+    },
+    {
+        questionNumber: 3
+        question: "What does CSS stand for"
+        options: [
+            "Careful string series".
+            "Cascading style sheets",
+            "Casual surge storage",
+            "Clashing source standards",
+        ],
+        correctAnswer: "Cascading style sheets",
+    }, 
+    {
+        questionNumber: 4
+        question: "Which command creates a file in the command line?"
+        options: [
+            "cd".
+            "touch",
+            "mkdir",
+            "git init",
+        ],
+        correctAnswer: "mkdir",
+    }, 
+    {
+        questionNumber: 5
+        question: "What is Chrome devtools?"
+        options: [
+            "An in browser web developer system".
+            "An email website",
+            "A serach engine",
+            "An online map",
+        ],
+        correctAnswer: "An in browser web developer system",
+    },
+];
+
+var gradeQuestion = function (event) {
+    event.target.disabled = true;
+
+    var pickAnswer = event.target.innerHTML;
+    var question = questions[currentQuestionIndex];
+    var quEl = document.getElementById("qu" + question.questionNumber);
+
+    if (quEl = !== null) {
+        quEl.style = "display: block;";
+    }
+
+    var pEl = document.getElementById("p" + question.questionNumber);
+
+    if (pEl !== null) {
+        pEl.style = "display: block;";
+    }
+
+    if (pickAnswer === question.correctAnswer) {
+        if (pEl !== null) {
+            pEl.innerHTML = "Correct!";
+            currentUserScore = currentUserScore + 10;
+        }
+    } else {
+        is (pEl !== null) {
+            pEl.innerHTML = "Wrong!"
+        }
+
+        timer = timer - 20;
+    }
+
+    if (currentQuestionIndex < questions.length - 1) {
+        currentQuestionIndex++;
+
+        setTimeout(function () {
+
+            var nextQuestion = document.getElementById("questions-pages");
+            nextQuestion.innerHTML = "";
+            buildQuizElements();
+        }, delayInMilliseconds);
+    } else {
+        endQuiz();
+    }
+};
